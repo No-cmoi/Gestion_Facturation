cc0e2d78083865bf6e392187295e6710cb56fb35
No-cmoi <arno.muller@free.fr>
2025-02-18 16:00:05 +0100

    Remove all tracked files and update .gitignore

--- Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace Gestion_Facturation.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
